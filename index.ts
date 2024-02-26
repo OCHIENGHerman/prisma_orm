c3c110b7dd43e319219e189d676e314da5687ad8
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-26 15:36:33 +0300

    add post and get user logics

--- index.ts
+++ index.ts
@@ -5,7 +5,28 @@ const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
-async function main() {
+
+app.post('/users', async (req, res) => {
+  const { name, email, posts } = req.body
+
+  const postData = posts?.map((post: Prisma.PostCreateInput) => {
+    return { title: post, content: post?.content}
+  })
+
+  const result = await prisma.user.create({
+    data: {name, email,
+      posts: {
+        create: postData,
+      },
+    },
+  })
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+//async function main() {
   // ... you will write your Prisma Client queries here
 //   await prisma.user.create({
 //     data: {
@@ -28,19 +49,19 @@ async function main() {
 //   })
 //   console.dir(allUsers, {depth: null})
 
-const post = await prisma.post.update({
-    where: { id: 1 },
-    data: { published: true },
-  })
-  console.log(post)
-}
+// const post = await prisma.post.update({
+//     where: { id: 1 },
+//     data: { published: true },
+//   })
+//   console.log(post)
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
