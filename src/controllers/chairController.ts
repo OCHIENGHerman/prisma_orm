d8a6f8fcf96b25fead1c36341109df8f66781421
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-26 23:46:15 +0300

    setup chair conntroller

--- src/controllers/chairController.ts
+++ src/controllers/chairController.ts
@@ -2,13 +2,30 @@ import { Request, Response } from 'express';
 import { PrismaClient } from ".prisma/client";
 
 const prisma = new PrismaClient();
-
-export const getAllUsers = async (req: Request, res: Response) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
-};
-
 export const getAllChairs = async (req: Request, res: Response) => {
     const chairs = await prisma.chair.findMany();
     res.json(chairs);
+}
+
+export const getChairById = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const chair = await prisma.chair.findUnique({ where: { id } });
+    res.json(chair);
+}
+
+export const createChair = async (req: Request, res: Response) => {
+    const chair = await prisma.chair.create({ data: req.body });
+    res.json(chair);
+}
+
+export const updateChair = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const chair = await prisma.chair.update({ where: { id }, data: req.body });
+    res.json(chair);
+}
+
+export const deleteChair = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const chair = await prisma.chair.delete({ where: { id } });
+    res.json(chair);
 }
