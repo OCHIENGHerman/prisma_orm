613c83335a3dd35fabf3204197598b01be0e1b24
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-26 19:26:37 +0300

    intergreat prisma within my app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
 
 const app = express();
 const port: number = 3000;
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
