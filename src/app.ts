3abd69957a41ebfcbad133fd779fc8466caa5c75
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-27 21:14:17 +0300

    use post routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import chairRoutes from './routes/chairRoutes';
 import userRoutes from './routes/userRoutes';
+import postRoutes from './routes/postRoutes';
 
 const app = express();
 const port: number = 3000;
@@ -8,11 +9,12 @@ const port: number = 3000;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Server Running!');
 });
 
 app.use('/chairs', chairRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 
 app.listen(port, () => {
