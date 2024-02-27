fab243ff321911015289f8f999fb1d1ed7686228
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-27 21:46:00 +0300

    add getpostsby user id routes

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -5,6 +5,8 @@ const router = express.Router()
 
 router.get('/', PostController.getAllPosts)
 router.get('/:id', PostController.getPostById)
+// New route to get posts by user ID
+router.get('/user/:userId', PostController.getPostsByUserId);
 router.post('/', PostController.createPost)
 router.put('/:id', PostController.updatePost)
 router.delete('/:id', PostController.deletePost)
