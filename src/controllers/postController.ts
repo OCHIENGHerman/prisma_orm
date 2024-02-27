7844254544f4c1d5cdaa04eb22614803173fc8c0
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-27 22:32:09 +0300

    minor fix imports

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import { validatePostData } from 'src/validations/postValidation';
+import { validatePostData } from '../validations/postValidation';
 
 const prisma = new PrismaClient();
 
